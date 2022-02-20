7a7aad8f04ff1bfdbb1c4b0d54856193fcbc3411
yuriquezada <quezadat.yuri@gmail.com>
2022-02-20 09:39:55 -0500

    Add routes in ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+        <Route path="about" element={} />
+      </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
